845df0e0a7540c6fe0fd71b39e51808bac868be5
mullerdj <mullerdj29@gmail.com>
2024-11-13 17:51:59 -0600

    header- footer

---
+++ netflix-clone/src/pages/home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Header from '../../components/header/Header'
+
+import  Footer from "../../components/footer/Footer"
+
+function Home() {
+  return (
+    <>
+        <Header/>
+        < Footer/>
+
+
+
+    </>
+  )
+}
+
+export default Home
