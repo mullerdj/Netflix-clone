fb6e02ae524beb9097bc7955d4977b4199d93828
mullerdj <mullerdj29@gmail.com>
2024-11-13 20:51:51 -0600

    row

--- netflix-clone/src/pages/home/Home.js
+++ netflix-clone/src/pages/home/Home.js
@@ -3,11 +3,15 @@ import React from 'react'
 import Header from '../../components/header/Header'
 
 import  Footer from "../../components/footer/Footer"
+import Banner from '../../components/banner/Banner'
+import Rowlist from '../../components/rows/rowlist/Rowlist'
 
 function Home() {
   return (
     <>
         <Header/>
+        <Banner/>
+        <Rowlist/>
         < Footer/>
 
 
